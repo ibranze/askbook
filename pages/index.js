6d528b224bc5c31688d0800444288e77c3793e2e
İbrahim Kaya <ottomanjr2434@gmail.com>
2022-03-01 14:37:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          asdasdsad
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/43DOTHM390514FWchGO2T7?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
